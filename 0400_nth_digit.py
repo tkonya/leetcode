6fe797ed2d3e0c7b2886d34c05197e0042e29961
T Konya <trevor.konya@gmail.com>
2018-12-26 16:30:12 -0500

    cleaned up and commented

--- 0400_nth_digit.py
+++ 0400_nth_digit.py
@@ -9,44 +9,32 @@ def findNDigit(n):
     n_end = 9  # highest n value in the block, inclusive
     digits_per = 1  # number of digits per real number in the block
     start_real_number = 1  # the real number this block starts at
-    digits_passed = 0  # the
 
-    # print('n start = ' + str(n_start) + ', n end = ' + str(n_end) + ', start_real = ' + str(start_real_number) + ', digits per = ' + str(digits_per) + ', passed = ' + str(digits_passed))
-    while n > n_end:
-        n_start = n_end + 1
-        go_up_by = ((start_real_number * 10) - start_real_number)
-        # digits_passed += (digits_per * go_up_by)
-        go_up_by *= 10
+    # n_end goes up very fast, so this loop does not run many times
+    while n > n_end:  # while n is still outside the range of our block
+        n_start = n_end + 1  # will now start at the next value
+        go_up_by = ((start_real_number * 10) - start_real_number) * 10
         start_real_number *= 10
-        digits_per += 1
+        digits_per += 1  # every next block has one more digit per real number
         n_end = (go_up_by * digits_per) + n_start - 1
-        # print('n start = ' + str(n_start) + ', n end = ' + str(n_end) + ', start_real = ' + str(start_real_number) + ', digits per = ' + str(digits_per) + ', passed = ' + str(digits_passed))
 
-    n_to_go_up = n - n_start
-    real_to_go_up = math.floor(n_to_go_up / digits_per)
+    # calculate what real number our n value will land on
+    real_to_go_up = math.floor((n - n_start) / digits_per)
     n_to_go_up = real_to_go_up * digits_per
-    # print('n up = ' + str(n_to_go_up) + ' real up ' + str(real_to_go_up))
-
     real_target = start_real_number + real_to_go_up
-    # print('real target = ' + str(real_target))
 
+    # calculate where to slice the real number
     index = n - (n_start + n_to_go_up)
-    # print('index = ' + str(index))
     return int(str(real_target)[index:index+1])
 
 
 # for testing only, really slow
 def findNthDigitDumb(n):
-    """
-    :type n: int
-    :rtype: int
-    """
     num_string = ''
     i = 0
     while len(num_string) < n:
         i += 1
         num_string += str(i)
-    print('n = ' + str(n) + ', last number added: ' + str(i) + ', answer = ' + num_string[n-1:n])
 
     return int(num_string[n-1:n])
 
